450b536ed8702b0badf66f62550715f4218fbcd0
Vinaya Srri Dasari <vinayasrridasari@Vinayas-MacBook-Pro.local>
2025-03-19 20:51:42 -0500

    s3 bucket through terraform

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "test-bucket"  # Change to a globally unique name
+  bucket = "test-bucket-2"  # Change to a globally unique name
   acl    = "private"
 
   tags = {
