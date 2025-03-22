9600b8c12c483fd8efc204b21c5261f6cc7040a0
Vinaya Srri Dasari <vinayasrridasari@Vinayas-MacBook-Pro.local>
2025-03-22 08:14:28 -0500

    s3 bucket through terraform

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
  }
  
 resource "aws_s3_bucket" "example" {
-   bucket = "test-bucket-terraform_vinaya"  # Replace with a unique bucket name
+   bucket = "test-bucket-terraform-vinaya"  # Replace with a unique bucket name
  
    tags = {
      Name        = "My Terraform S3 Bucket"
