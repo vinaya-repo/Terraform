741ef04cfb3bb6b15dd812464dda63d3f6aca377
Vinaya Srri Dasari <vinayasrridasari@Vinayas-MacBook-Pro.local>
2025-03-22 07:20:50 -0500

    s3 bucket through terraform

--- main.tf
+++ main.tf
@@ -2,22 +2,9 @@ provider "aws" {
    region = "us-east-1"  # Replace with your AWS region
  }
  
-# resource "aws_s3_bucket" "my_bucket" {
-#    bucket = "temp-bucket-terraform"  # Replace with a unique bucket name
-#    acl    = "private"
- 
-#    tags = {
-#      Name        = "My Terraform S3 Bucket"
-#      Environment = "Dev"
-#    }
-#  }
- 
-#  output "s3_bucket_name" {
-#    value = aws_s3_bucket.my_bucket.bucket
-#  }
-
-resource "aws_s3_bucket" "backend_bucket" {
+resource "aws_s3_bucket" "my_bucket" {
    bucket = "s3-backend-terraform"  # Replace with a unique bucket name
+   acl    = "private"
  
    tags = {
      Name        = "My Terraform S3 Bucket"
@@ -25,8 +12,8 @@ resource "aws_s3_bucket" "backend_bucket" {
    }
  }
  
- output "s3_backend_bucket_name" {
-   value = aws_s3_bucket.backend_bucket.bucket
+ output "s3_bucket_name" {
+   value = aws_s3_bucket.my_bucket.bucket
  }
 
 resource "aws_dynamodb_table" "terraform_locks" {
