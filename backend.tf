36e0e626ee01e51f39edde79f5fb973e81e6d476
Vinaya Srri Dasari <vinayasrridasari@Vinayas-MacBook-Pro.local>
2025-03-22 06:23:27 -0500

    s3 bucket through terraform

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "my-terraform-state-bucket"  # Replace with your unique bucket name
     key            = "terraform.tfstate"          # Path of the state file in S3
     region         = "us-east-1"                  # Replace with your region
-    dynamodb_table = "terraform-lock"            # DynamoDB table for state locking
+    use_lockfile   = true            # DynamoDB table for state locking
     encrypt        = true                         # Encrypt the state file
   }
 }
